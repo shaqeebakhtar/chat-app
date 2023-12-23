cbd50acf9f1a1e7694c71a4becdd739ec5291429
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-12-23 20:05:27 +0530

    add pub/sub for sending messages

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server, type Socket } from 'socket.io';
+import process from 'process';
+import { Redis } from 'ioredis';
 
 export const startServer = () => {
+  const publisher = new Redis();
+  const subscriber = new Redis();
+
   const port = process.env.PORT || 8000;
 
   const app = express();
@@ -17,8 +22,15 @@ export const startServer = () => {
   io.on('connection', (socket: Socket) => {
     console.log('New client connected:', socket.id);
 
-    socket.on('message', ({ message }) => {
-      io.emit('message', message);
+    socket.on('message', async ({ message }) => {
+      await publisher.publish('MESSAGES', JSON.stringify({ message }));
+    });
+  });
+
+  subscriber.subscribe('MESSAGES', () => {
+    subscriber.on('message', (channel, message) => {
+      if (channel === 'MESSAGES')
+        io.emit('message', JSON.parse(message).message);
     });
   });
 
--- apps/web/app/chat/_components/chat-messages.tsx
+++ apps/web/app/chat/_components/chat-messages.tsx
@@ -7,15 +7,11 @@ const ChatMessages = () => {
   return (
     <div className=" h-[calc(100vh-90px)] p-6">
       <div className="space-y-1">
-        {messages.map((msg) => (
-          <div className="bg-white w-max px-4 p-2 shadow-sm rounded-r-3xl rounded-b-3xl rounded-l-none">
-            {msg}
-          </div>
-        ))}
-      </div>
-      <div className="space-y-1">
-        {messages.map((msg) => (
-          <div className="bg-white ml-auto w-max px-4 p-2 shadow-sm rounded-l-3xl rounded-b-3xl rounded-r-none">
+        {messages.map((msg, idx) => (
+          <div
+            key={idx}
+            className="bg-white w-max px-4 p-2 shadow-sm rounded-r-3xl rounded-b-3xl rounded-tl-none"
+          >
             {msg}
           </div>
         ))}
